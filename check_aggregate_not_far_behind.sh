0d28d79d058465dd606b6c8bd5c8e3fb95b94f13
lukaw3d <lukajeran@oasislabs.com>
2024-01-20 04:18:37 +0100

    Print more in indexer-analyzers-are-running

--- check_aggregate_not_far_behind.sh
+++ check_aggregate_not_far_behind.sh
@@ -1,8 +1,13 @@
+#!/bin/bash
+set -euo pipefail
+set -x
+
 status_url=$1
 aggregate_url=$2
-latest_time_str=$(curl "$status_url" --fail | jq .latest_block_time -r)
-latest_time=$(date -d $latest_time_str +%s)
-aggregate_stats_time_str=$(curl "$aggregate_url" --fail | jq .windows[0].window_end -r)
-aggregate_stats_time=$(date -d $aggregate_stats_time_str +%s)
+latest_time_str=$(curl "$status_url" -sS --fail | jq .latest_block_time -r)
+aggregate_stats_time_str=$(curl "$aggregate_url" -sS --fail | jq .windows[0].window_end -r)
 echo "is $aggregate_stats_time_str more than 12 hours behind $latest_time_str?"
+
+latest_time=$(date -d $latest_time_str +%s)
+aggregate_stats_time=$(date -d $aggregate_stats_time_str +%s)
 [ $aggregate_stats_time -ge $(( $latest_time - 12*60*60 )) ]
